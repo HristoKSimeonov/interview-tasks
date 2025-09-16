50aa5cc17ce1666dc2794f7bcc454c53f998b350
Hristo Simeonov <buddhlaren@gmail.com>
2025-09-16 22:00:43 +0300

    adding the secret suffix

--- infrastructure-automation/kms.tf
+++ infrastructure-automation/kms.tf
@@ -11,7 +11,7 @@ resource "aws_kms_key" "db_secret_key" {
 
 # KMS Key Alias
 resource "aws_kms_alias" "db_secret_key_alias" {
-  name          = "alias/${var.project_name}-${var.environment}-db-secret-v2"
+  name          = "alias/${var.project_name}-${var.environment}-db-secret-${random_id.secret_suffix.hex}"
   target_key_id = aws_kms_key.db_secret_key.key_id
 }
 
@@ -22,15 +22,20 @@ resource "random_password" "db_master_password" {
   override_special = "!#$%&*+-=?^_`{|}~"
 }
 
+# Generate random suffix for secret name
+resource "random_id" "secret_suffix" {
+  byte_length = 4
+}
+
 # Secrets Manager Secret
 resource "aws_secretsmanager_secret" "db_credentials" {
-  name                    = "${var.project_name}-${var.environment}-db-credentials"
+  name                    = "${var.project_name}-${var.environment}-db-credentials-${random_id.secret_suffix.hex}"
   description             = "Database master credentials for RDS PostgreSQL"
   kms_key_id              = aws_kms_key.db_secret_key.arn
   recovery_window_in_days = 7
 
   tags = {
-    Name = "${var.project_name}-${var.environment}-db-credentials-v2"
+    Name = "${var.project_name}-${var.environment}-db-credentials-${random_id.secret_suffix.hex}"
   }
 }
 
